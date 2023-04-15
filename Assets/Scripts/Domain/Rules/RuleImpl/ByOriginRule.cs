cdc6db28ff1dea478f402e144270876380b24beb
Jacinto Mba Cantero <jacmba@gmail.com>
2023-04-15 19:07:42 +0100

    Selection rules implementation

---
+++ Assets/Scripts/Domain/Rules/RuleImpl/ByOriginRule.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ByOriginRule : IRule
+{
+    private OriginType origin;
+
+    public ByOriginRule()
+    {
+        int iOrigin = new Random().Next((int)OriginType.N, (int)OriginType.W);
+        origin = (OriginType)iOrigin;
+    }
+    public string Stringify()
+    {
+        return "Origin: " + origin;
+    }
+
+    public bool Validate(Candidate candidate, DateTime currentDate)
+    {
+        return candidate.origin == origin;
+    }
+}
