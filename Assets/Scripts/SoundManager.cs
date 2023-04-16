85344b6a53571dd586e93f5288d09c2bb7d1f49e
Jacinto Mba Cantero <jacmba@gmail.com>
2023-04-16 15:15:26 +0100

    WIP Sound Manager Prefab

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,10 +6,73 @@ public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioSource fxSource;
     [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource backgroundSource;
     [SerializeField] private AudioClip playClip;
     [SerializeField] private AudioClip startClip;
     [SerializeField] private AudioClip passClip;
     [SerializeField] private AudioClip denyClip;
+    [SerializeField] private AudioClip rulesShowClip;
+    [SerializeField] private AudioClip pauseClip;
+    [SerializeField] private AudioClip exitClip;
+    [SerializeField] private AudioClip validClip;
+    [SerializeField] private AudioClip notValidClip;
+    [SerializeField] private AudioClip trustSlideClip;
+    [SerializeField] private AudioClip hiScoreClip;
+    [SerializeField] private AudioClip backGroundClip;
+
+    public void StartBackgroundNoise()
+    {
+        backgroundSource.clip = backGroundClip;
+        backgroundSource.Play();
+    }
+
+    public void StopBackgroundNoise()
+    {
+        backgroundSource.Stop();
+    }
+
+    public void TrustSlide()
+    {
+        fxSource.clip = trustSlideClip;
+        fxSource.Play();
+    }
+
+    public void HiScore()
+    {
+        fxSource.clip = hiScoreClip;
+        fxSource.Play();
+    }
+
+
+    public void ShowRules()
+    {
+        fxSource.clip = rulesShowClip;
+        fxSource.Play();
+    }
+
+    public void Pause()
+    {
+        fxSource.clip = pauseClip;
+        fxSource.Play();
+    }
+
+    public void Exit()
+    {
+        fxSource.clip = exitClip;
+        fxSource.Play();
+    }
+
+    public void Valid()
+    {
+        fxSource.clip = validClip;
+        fxSource.Play();
+    }
+
+    public void NotValid()
+    {
+        fxSource.clip = notValidClip;
+        fxSource.Play();
+    }
 
     public void Play()
     {
